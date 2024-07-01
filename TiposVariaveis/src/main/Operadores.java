9dcca5fda17c5ac567c1744632a600e4b76d859d
sadfrogbr <samueltonis10@gmail.com>
2024-07-01 17:55:26 -0300

    operadores

--- TiposVariaveis/src/main/Operadores.java
+++ TiposVariaveis/src/main/Operadores.java
@@ -3,14 +3,41 @@ package main;
 public class Operadores {
 
 	public static void main(String[] args) {
-		boolean variavel =true;
+//		boolean variavel =true;
+//		int numero = 1;
+//		System.out.println(!variavel);
+//		System.out.println(variavel);
+//		
+//		
+//		String resultado = numero == 1 ? "verdade" : "mentira";
+//		System.out.println(resultado);
+
+		
+//		int numero1 = 1;
+//		int numero2 = 2;
+//		
+//		if(numero1 < numero2) {
+//			System.out.println("jojo");
+//		}
+//		
+//		boolean simNao = numero1 == numero2;
+//		
+//		System.out.println(simNao);
+		
+//		String nomeUm = "samuel";
+//		String nomeDois = "samuel";
+//		
+//		System.out.println(nomeUm.equals(nomeDois));
+		
+		
 		int numero = 1;
-		System.out.println(!variavel);
-		System.out.println(variavel);
+		
+		if(1 == 1 || numero  == 2) {
+			System.out.println("entrou");
+		}
 		
 		
-		String resultado = numero == 1 ? "verdade" : "mentira";
-		System.out.println(resultado);
+		
 		
 	}
 
